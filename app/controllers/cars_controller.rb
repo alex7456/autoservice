cb063b210f50ce418ba37910404705142d2f2a24
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-01-22 12:17:12 +0300

    Исправление недочетов

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,11 +1,10 @@
 class CarsController < ApplicationController
-  before_action :set_car, only: [:show, :edit, :update, :destroy]
-
   def index
     @cars = Car.all
   end
 
   def show
+    @car = Car.find(params[:id])
   end
 
   def new
@@ -22,26 +21,25 @@ class CarsController < ApplicationController
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    @car = Car.find(params[:id])
     if @car.update(car_params)
-      redirect_to cars_path, notice: "Данные об автомобиле обновлены!"
+      redirect_to cars_path, notice: "Автомобиль успешно обновлён!"
     else
       render :edit
     end
   end
 
   def destroy
+    @car = Car.find(params[:id])
     @car.destroy
-    redirect_to cars_path, notice: "Автомобиль удален."
+    redirect_to cars_path, notice: "Автомобиль удалён!"
   end
 
   private
-
-  def set_car
-    @car = Car.find(params[:id])
-  end
 
   def car_params
     params.require(:car).permit(:license_plate, :brand, :model, :year)
